ae1bc5c634414a276b6122699f7c0cba68c6be83
Paul Rock <paul@drteam.rocks>
2023-12-25 18:57:59 +0300

    Dataset generating update

--- simple_gnn/graph/generate_subgraphs.py
+++ simple_gnn/graph/generate_subgraphs.py
@@ -1,4 +1,6 @@
 import random
+import torch
+from torch_geometric.data import Data
 
 
 def generate_subgraphs(dataset, num_subgraphs=5, min_nodes=2, max_nodes=5):
@@ -22,6 +24,41 @@ def generate_subgraphs(dataset, num_subgraphs=5, min_nodes=2, max_nodes=5):
                           link['source'] in selected_node_ids and link['target'] in selected_node_ids]
         subgraphs.append({'nodes': selected_nodes, 'links': selected_links})
     return subgraphs
+
+
+def generate_subgraphs_dataset(subgraphs, large_dataset):
+    # Generate dataset from all subgraphs
+    dataset = []
+    for i in range(len(subgraphs)):
+        user_edge_index = []
+        for link in subgraphs[i]['links']:
+            source_idx = large_dataset.node_mapping.get(link['source'])
+            target_idx = large_dataset.node_mapping.get(link['target'])
+            # Add edge only if both nodes are on the subgraph
+            if source_idx is not None and target_idx is not None:
+                user_edge_index.append([source_idx, target_idx])
+        user_edge_index = torch.tensor(user_edge_index, dtype=torch.long).t().contiguous()
+
+        # Convert subgraphs nodes of the small graph
+        user_node_index = []
+        for link in subgraphs[i]['nodes']:
+            node_idx = large_dataset.node_mapping.get(link['id'])
+            if node_idx is not None:
+                user_node_index.append(node_idx)
+        user_node_indices = large_dataset.x[user_node_index]
+
+        # Make a mask for the subgraph nodes
+        user_mask = torch.zeros_like(large_dataset.x)
+        for idx in user_node_index:
+            user_mask[idx] = 1
+        masked_features = large_dataset.x * user_mask
+
+        # Create a dataset from the subgraph using the same features and labels as the original dataset
+        user_data = Data(x=masked_features, edge_index=user_edge_index, y=large_dataset.y)
+
+        dataset.append(user_data)
+
+    return dataset
 
 
 if __name__ == "__main__":
